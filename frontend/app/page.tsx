9fd3a1dbde81df1025c61e021a005239c9fc2a3b
fujianmian <junyong112204@gmail.com>
2025-05-02 00:39:40 +0800

    first try, just change a logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-custom-yellowwhite">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/GlobeCoin_WithName.jpeg"
+          alt="Globe Coin logo"
           width={180}
           height={38}
           priority
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      'custom-yellowwhite': 'rgb(245, 244, 239)',
+    },
+  },
+  plugins: [],
+}
+
